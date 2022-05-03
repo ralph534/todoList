404cc526ba8faee4d5b5706622ac184867951d4f
Ralph Etienne <ralphyetienne@yahoo.com>
2022-05-03 00:09:31 -0400

    created handleSubmit function to prevent app refreshing when Add Todo is clicked

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,13 +1,27 @@
 import React, {useState} from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
   const [input, setInput] = useState('');
+
+///  This function is to make changes in a specifc area in the App (FORM) tag
+///   without refreshing the whole web browser
+  const handleSubmit =  (e) => {
+    e.preventDefault();
+  }
+
+
+///  This function changes the default state of input which is an empty string
+///  given setInput as the second param in the usestate to target the value in the 
+////  input field
+  const inputChanger = (e) => {
+      setInput(e.target.value);
+  }
 
   
   return (
     <div>
-         <form className='todo-form'>
-             <input type='text' placeholder='WHAT TO DO?' value={input} name='text' className='todo-input' />
+         <form className='todo-form' onSubmit={handleSubmit}>
+             <input type='text' placeholder='WHAT TO DO?' value={input} name='text' className='todo-input' onChange={inputChanger} />
              <button className='todo-button'>ADD TODO</button>
 
          </form>
